22733dc26c31203ac030903a569b356039972847
codeypas <51842054+codeypas@users.noreply.github.com>
2024-02-18 14:01:30 +0530

    profileImageUploadFunctionality

--- viot/src/component/DashProfile.jsx
+++ viot/src/component/DashProfile.jsx
@@ -1,30 +1,111 @@
-// export default function DashProfile() {
-//   return (
-//     <div>
-//       <h1>dashprofile</h1>
-//     </div>
-//   )
-// }
-
-
-
-
-
-
-import { useSelector } from 'react-redux'
+import { Alert, Button, TextInput } from 'flowbite-react';
+import { useEffect, useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
+import {getDownloadURL, getStorage, ref, uploadBytes, uploadBytesResumable} from 'firebase/storage';
+import { app } from '../firebase';
 
 export default function DashProfile() {
     const { currentUser } = useSelector(state => state.user)
+    const [imageFile,setImageFile]=useState(null);
+    const [imageFileUrl,setImageFileUrl]=useState(null);
+    const[imageFileUploadProgress,setImageFileUploadProgress]=useState(null);
+    const[imageFileUploadError,setImageFileUploadError]=useState(null);
+    // console.log(imageFileUploadProgress,imageFileUploadError);
+    const filePickerRef=useRef();
+
+    const handelImageChange=(e)=>{
+        const file=e.target.files[0]
+        if(file){
+            setImageFile(file);
+            setImageFileUrl(URL.createObjectURL(file));
+        }
+
+    };
+    useEffect(()=>{
+        if(imageFile){
+            uploadImage();
+        }
+    },[imageFile]);
+const uploadImage=async()=>{
+
+//firebase storage code
+    // service firebase.storage {
+    //     match /b/{bucket}/o {
+    //       match /{allPaths=**} {
+    //         allow read;
+    //         allow write: if
+    //         request.resource.size<2*1024*1024 &&
+    //         request.resource.contentType.matches('image/.*')
+    //       }
+    //     }
+    //   }
+
+    setImageFileUploadError(null);
+    const storage=getStorage(app);
+    const fileName=new Date().getTime()+imageFile.name;
+    const storageRef=ref(storage,fileName);
+    const uploadTask =uploadBytesResumable(storageRef,imageFile);
+    uploadTask.on(
+        'state_changed',
+        (snapshot)=>{
+            const progress=(snapshot.bytesTransferred/snapshot.totalBytes)*100;
+            setImageFileUploadProgress(progress.toFixed(0));
+        },
+        (error)=>{
+            setImageFileUploadError('could not upload image plz make file size less then 2MB'),
+            setImageFile(null);
+            setImageFileUrl(null);
+        },
+
+        ()=>{
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=>{
+                setImageFileUrl(downloadURL);
+            });
+        });
+};
+
     return (
-        <div className='amx-w-lg mx-auto p-3 w-full'>
+        <div className='max-w-lg mx-auto p-3 w-full'>
             <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
-            <form className='flex flex-col'>
-                <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'>
-                    <img src={currentUser.profilePicture} 
+            <form className='flex flex-col gap-5'>
+                <input type='file' accept='image/*' onChange={handelImageChange} ref={filePickerRef}
+                hidden
+                />
+                <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full' onClick={()=>
+                    filePickerRef.current.click()
+                }>
+                    <img src={imageFileUrl || currentUser.profilePicture} 
                     alt="user"
                     className='rounded-full w-full h-full object-cover border-8' />
                 </div>
+                {imageFileUploadError && <Alert color='failure'>
+                    {imageFileUploadError}
+                </Alert>}
+
+                <TextInput type='text' 
+                id='username' 
+                placeholder='username' 
+                defaultValue={currentUser.username}/>
+
+                <TextInput type='email' 
+                id='email' 
+                placeholder='email' 
+                defaultValue={currentUser.email}/>
+
+                <TextInput 
+                type='password' 
+                id='password' 
+                placeholder='password'/>
+
+                <Button type='submit' color='blue' outline>Update</Button>
+
+
+
             </form>
+            <div className='text-red-400 flex justify-between mt-4 '>
+                <span className='cursor-pointer'>Delete Account</span>
+                <span className='cursor-pointer'>Sign Out</span>
+            </div>
         </div>
     );
 }
